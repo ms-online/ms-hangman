cc102825829ae8c2d2445391c62b42adea66ee44
ms-online <27732357@qq.com>
2020-03-03 17:19:10 +0800

    lesson-5

---
+++ script.js
@@ -0,0 +1,41 @@
+// 第一步 获取节点
+const wordEl = document.getElementById("word");
+const wrongLettersEl = document.getElementById("wrong-letters");
+const playAgainBtn = document.getElementById("play-button");
+const popup = document.getElementById("popup-container");
+const notification = document.getElementById("notification-container");
+const finalMessage = document.getElementById("final-message");
+
+const figureParts = document.querySelectorAll(".figure-part");
+
+const words = ["application", "programming", "interface", "wonder"];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+// console.log(selectedWord);
+
+const correctLetters = [];
+const wrongLetters = [];
+
+// 显示单词函数
+function displayWord() {
+  wordEl.innerHTML = `
+    ${selectedWord
+      .split("")
+      .map(
+        letter => `
+        <span class="letter">
+        ${correctLetters.includes(letter) ? letter : ""}
+        </span>
+        `
+      )
+      .join("")}
+    `;
+  const innerWord = wordEl.innerText.replace(/\n/g, "");
+
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = "恭喜你输入正确！ 😃";
+    popup.style.display = "flex";
+  }
+}
+
+displayWord();
